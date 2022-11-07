c2e26b6b28ce0555cb7571eadc6c9060f0d88ffb
Atieh RasouliAhari <rasouli.a@tiddev.com>
2022-11-07 16:11:02 +0330

    Add project files.

---
+++ ConsoleApp4/PresentNewFeatureClass.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp4
+{
+    internal static class PresentNewFeatureClass
+    {
+        public static bool IsLetter(this char c) =>
+                 c is >= 'a' and <= 'z' or >= 'A' and <= 'Z';
+
+
+        public static bool IsLetterOrSeparator(this char c) =>
+               c is (>= 'a' and <= 'z') or (>= 'A' and <= 'Z') or '.' or ',';
+
+
+        public static bool IsConferenceDay(DateTime date) =>
+            date is { Year: 2020, Month: 5, Day: 19 or 20 or 21 };
+
+
+        public record Point(int X, int Y);
+
+        public static Point Transform(Point point) => point switch
+        {
+            var (x, y) when x < y => new Point(-x, y),
+            var (x, y) when x > y => new Point(x, -y),
+            var (x, y) => new Point(x, y),
+        };
+
+        static bool IsAcceptable(int id, int absLimit) =>
+                         SimulateDataFetch(id) is var results
+                         && results.Min() >= -absLimit
+                         && results.Max() <= absLimit;
+
+        static int[] SimulateDataFetch(int id)
+        {
+            var rand = new Random();
+            return Enumerable
+                       .Range(start: 0, count: 5)
+                       .Select(s => rand.Next(minValue: -10, maxValue: 11))
+                       .ToArray();
+        }
+        public static bool IsNullTest(object e)
+        {
+            if (e is not null)
+            {
+               return true;
+            }
+            else
+                
+                return false;
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp4;
+using System.Collections.ObjectModel;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(nint.MaxValue);
+Console.WriteLine(uint.MaxValue);
+Console.WriteLine($"nint.MinValue = {nint.MinValue}");
+Console.WriteLine($"nint.MaxValue = {nint.MaxValue}");
+Console.WriteLine($"nuint.MinValue = {nuint.MinValue}");
+Console.WriteLine($"nuint.MaxValue = {nuint.MaxValue}");
+
+
+
+
+var a = PresentNewFeatureClass.IsLetter(Convert.ToChar("j"));
+var b = PresentNewFeatureClass.IsLetter(Convert.ToChar("5"));
+
+
+var se = PresentNewFeatureClass.IsLetterOrSeparator(Convert.ToChar("l"));
+var se2 = PresentNewFeatureClass.IsLetterOrSeparator(Convert.ToChar("."));
+
+
+var bb = PresentNewFeatureClass.IsConferenceDay(DateTime.Now);
+
+
+
+Console.WriteLine(PresentNewFeatureClass.Transform(new PresentNewFeatureClass.Point(1, 2)));  // output: Point { X = -1, Y = 2 }
+Console.WriteLine(PresentNewFeatureClass.Transform(new PresentNewFeatureClass.Point(5, 2)));  // output: Point { X = 5, Y = -2 }
+
+var ff = PresentNewFeatureClass.IsNullTest(new object());
+var ff2 = PresentNewFeatureClass.IsNullTest(null);
+
+ObservableCollection<string> collection = new();
+Person person = new("Hermit", "Purple");
+
+Console.ReadKey();
